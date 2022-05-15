1a5dfce07ccd7778c4e7e753a1f81c70b1ff39d4
Gleison Batista <gleisonbs@gmail.com>
2022-05-14 22:13:31 -0300

    feat: add validations

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -5,16 +5,19 @@ pragma solidity >=0.5.0 <0.9.0;
 contract Lottery {
     address payable[] public players;
     address public manager;
+    uint constant public fee = 0.1 ether;
 
     constructor() {
         manager = msg.sender;
     }
 
     receive() external payable {
+        require(msg.value == fee);
         players.push(payable(msg.sender));
     }
 
     function getBalance() public view returns(uint) {
+        require(msg.sender == manager);
         return address(this).balance;
     }
 }
